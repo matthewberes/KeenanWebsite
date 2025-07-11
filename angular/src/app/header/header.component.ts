53402f82e7746e48eb370fc978087e51c4baa3b4
Matt Beres <60519715+matthewberes@users.noreply.github.com>
2025-07-11 14:05:03 -0400

    search result div skeleton

--- angular/src/app/header/header.component.ts
+++ angular/src/app/header/header.component.ts
@@ -1,18 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ToolbarComponent } from '../toolbar/toolbar.component';
 import { Router } from '@angular/router';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-header',
-  imports: [ToolbarComponent, NgIf],
+  imports: [ToolbarComponent, NgIf, NgFor, FormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
   constructor(private router: Router) { }
+
   searchClicked = false;
-  notEmpty = true;
+  isEmpty = true;
+  currText = "";
+  pages = ["Home", "Contact Us", "About Us", "Gallery"];
+  pageMap: Map<string, string> = new Map<string, string>();
+  pageResults: string[] = [];
+
+  ngOnInit(): void {
+    this.pageMap.set("Home", "/home");
+    this.pageMap.set("Contact Us", "/contact");
+    this.pageMap.set("About Us", "/about");
+    this.pageMap.set("Gallery", "/gallery");
+  }
 
   onClick() {
     this.router.navigate(['/home']);
@@ -20,5 +33,13 @@ export class HeaderComponent {
 
   onSearchClick() {
     this.searchClicked = true;
+  }
+
+  searchPages() {
+    const query = this.currText.trim().toLowerCase();
+    this.pageResults = this.pages.filter(page =>
+      page.toLowerCase().includes(query)
+    );
+    console.log(this.pageResults)
   }
 }
